5bf04f86c9ceaecc4e97959a5d0a1d9d85aea199
Cekepek <ckpk7171@gmail.com>
2023-03-15 11:28:19 +0700

    Week 3 Dialog fragment + drawer

--- app/src/main/java/com/cekepek/adv160420021week2/MainActivity.kt
+++ app/src/main/java/com/cekepek/adv160420021week2/MainActivity.kt
@@ -2,7 +2,9 @@ package com.cekepek.adv160420021week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
@@ -10,18 +12,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawerLayout)
+
         navController =
             (supportFragmentManager.findFragmentById(R.id.hostFragment) as
                     NavHostFragment).navController
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         bottomNav.setupWithNavController(navController)
+        NavigationUI.setupWithNavController(navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
+                || super.onSupportNavigateUp()
     }
 }
